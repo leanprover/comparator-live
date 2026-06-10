32607ec01b74e1cb29985d7933b29fba83ad5fdc
Robert J. Simmons <442315+robsimmons@users.noreply.github.com>
2026-06-10 12:16:17 -0400

    feat: use the backend verification, track recognized challenges (part 4 of 6)  (#5)
    - the frontend tracks which challenges are known ("trusted")
     - the backend validation matches challenges with solutions
    - simpleStatus.ts merges these two signals (do we trust the challenge,
    does the solution answer the challenge)

---
+++ frontend/src/store/simpleStatus.ts
@@ -0,0 +1,59 @@
+import { atom } from "jotai";
+
+import { type SimpleStatus } from "../utils/style.ts";
+import { recognitionStateAtom } from "./trusted.ts";
+import { comparatorResultAtom, isComparatorSyncedAtom } from "./verifier.ts";
+
+/**
+ * Atom for calculating the current global UI styling.
+ *
+ * NOTE: Avoid the temptation to use this output for any decision aside from
+ * applying a classname to a class. If you need anything else, directly access
+ * `isComparatorSyncedAtom` and `recognitionStateAtom`, or whatever
+ * information is most relevant to the task at hand.
+ */
+export const statusClassAtom = atom((get): SimpleStatus => {
+  const isComparatorSynced = get(isComparatorSyncedAtom);
+  const recognitionState = get(recognitionStateAtom);
+  if (!isComparatorSynced) return "stale";
+  if (!recognitionState) return "working";
+
+  const comparator = get(comparatorResultAtom);
+
+  switch (comparator.type) {
+    case "initial-load":
+    case "in-preparation":
+    case "in-progress":
+    case "in-queue": {
+      return "working";
+    }
+
+    case "verification-ok": {
+      if (comparator.theoremNames.length === 0) {
+        // Even if a user trusts a theorem-less development, it means
+        // comparator is effectively doing nothing, so we're going to
+        // give the error condition. (Due to how bubblewrap overlays
+        // things, this can also happen if a Challenge.olean appears in
+        // the project template's root folder, which is another reason
+        // to have this be an error.)
+        return "failure";
+      }
+      switch (recognitionState.type) {
+        case "user":
+          return "warning";
+        case "built-in":
+          return "success";
+        default:
+          // NB: verification success with an untrusted challenge
+          // results in *failure* UI state, not a warning state. We want
+          // to show red to the user in this case; we're treating it as
+          // if it's as dangerous as an failed verification.
+          return "failure";
+      }
+    }
+
+    default: {
+      return "failure";
+    }
+  }
+});
---
+++ frontend/src/store/trusted.ts
@@ -0,0 +1,81 @@
+import { atom } from "jotai";
+import { atomWithStorage, unwrap } from "jotai/utils";
+
+import { challengeAtom } from "./params.ts";
+
+const CHALLENGE_HASH_MS_DEBOUNCE = 800;
+
+/**
+ * SHA-256 hashes the challenge (end-trimmed with a trailing newline
+ * re-inserted).
+ *
+ * Uses promises and cancellation to debounce. The debounce is less about
+ * avoiding useless hashes, and more about making the delay before showing a
+ * challenge result look intentional instead of like an unpleasant flicker.
+ */
+export const challengeHashAtom = atom(async (get, { signal }) => {
+  const challenge = get(challengeAtom);
+  await new Promise((resolve) => setTimeout(resolve, CHALLENGE_HASH_MS_DEBOUNCE));
+  signal.throwIfAborted();
+
+  const encoder = new TextEncoder();
+  const data = encoder.encode(challenge.trimEnd() + "\n");
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return [...new Uint8Array(hash)]
+    .map((b: number): string => b.toString(16).padStart(2, "0"))
+    .join("");
+});
+
+const storeTrustedAtom = atomWithStorage<[string, string][]>("locallyTrusted", [], undefined, {
+  getOnInit: true,
+});
+
+/** The set of challenges the user has chosen to trust */
+export const locallyTrustedAtom = atom(
+  (get) => {
+    const stored = get(storeTrustedAtom);
+    return Object.fromEntries(stored);
+  },
+  (get, set, trusted: { [hash: string]: string }) => {
+    const keys = Object.keys(trusted);
+    set(
+      storeTrustedAtom,
+      keys.map((key) => [key, trusted[key]!]),
+    );
+  },
+);
+
+type TrustRecognition =
+  | { type: "built-in"; name: string; sources: string[] }
+  | { type: "user"; name: string }
+  | { type: "none" }
+  | { type: "empty" };
+
+/**
+ * Do we recognize this challenge as one of our trusted challenges, or one of
+ * the user's trusted challenges?
+ */
+export const recognitionAtom = atom<Promise<TrustRecognition>>(async (get) => {
+  if (get(challengeAtom).trim() === "") {
+    return { type: "empty" };
+  }
+
+  const challengeHash = await get(challengeHashAtom);
+
+  const { builtInTrusted } = await import("./builtInTrusted.ts");
+  const builtInTrust = builtInTrusted[challengeHash];
+  if (builtInTrust) return { type: "built-in", ...builtInTrust };
+
+  const locallyTrusted = get(locallyTrustedAtom)[challengeHash];
+  if (locallyTrusted) return { type: "user", name: locallyTrusted };
+
+  return { type: "none" };
+});
+
+/**
+ * Same as `recognitionAtom`, but allows a synchronous check on an unready
+ * promise (null means the promise is not ready).
+ *
+ * Implemented with [unwrap](https://jotai.org/docs/utilities/async#unwrap).
+ */
+export const recognitionStateAtom = unwrap(recognitionAtom, () => null);
