2bbb9c669df6b5e386759acb738274581e5a7ea9
Rob Simmons <rob@lean-fro.org>
2026-06-11 13:08:43 -0400

    feat: add trusted challenge for dependency-free flt

---
+++ Projects/mathlib-stable/TrustedChallenges/Fermat's Last Theorem (without Mathlib dependency).lean
@@ -0,0 +1,4 @@
+theorem flt : ∀ (x y z n : Nat),
+    x > 0 → y > 0 → z > 0 → n > 2 →
+    x^n + y^n ≠ z^n :=
+  by sorry
---
+++ Projects/mathlib-stable/TrustedChallenges/Fermat's Last Theorem.lean
@@ -0,0 +1,7 @@
+-- https://github.com/ImperialCollegeLondon/FLT/blob/main/FermatsLastTheorem.lean
+import Mathlib.Data.PNat.Basic
+theorem PNat.pow_add_pow_ne_pow
+    (x y z : ℕ+)
+    (n : ℕ) (hn : n > 2) :
+    x^n + y^n ≠ z^n :=
+  by sorry
